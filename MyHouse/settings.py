155720fb2a19ece3fe8fa9d2619e270091e53984
oroni <brianoroni6@gmail.com>
2016-12-06 13:08:34 +0300

    uptade requirements.txt

--- MyHouse/settings.py
+++ MyHouse/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!hei*5vm=o$8_i@@9_+^#vom^h5lbe&7=)*^zj2w(z%@n%%c9&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
